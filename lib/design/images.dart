6f36d95c32c2cbbef86fe68bbc89786edbdcf4e6
sewaustav <sewabobrov04@gmail.com>
2025-05-06 16:30:05 +0200

    update mainpage

--- lib/design/images.dart
+++ lib/design/images.dart
@@ -7,3 +7,5 @@ final SvgPicture profile = SvgPicture.asset('assets/images/profile.svg');
 final SvgPicture history_icon = SvgPicture.asset('assets/images/history-icon.svg');
 
 final SvgPicture history = SvgPicture.asset('assets/images/history.svg');
+
+final SvgPicture ai = SvgPicture.asset('assets/images/ai.svg');
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutterfitapp/design/colors.dart';
 import 'package:go_router/go_router.dart';
 import 'package:flutterfitapp/design/images.dart';
 
@@ -8,9 +9,46 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Home')),
-      body: Container(
-        child: Text('Главная'),
+      appBar: AppBar(
+          title: Text('Home', style:TextStyle(
+            color: Colors.white,
+          )),
+          backgroundColor: MyColors.blue_color,
+      ),
+      body: Center(
+          child: Container(
+            margin: EdgeInsets.all(20),
+            child: Column(
+
+            children: [
+              Container(
+                margin: EdgeInsets.all(15),
+                child: Text("Welcome guest!", style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 24,
+                  fontWeight: FontWeight.w600,
+              ),),),
+              TextButton(
+                  onPressed: () => context.go('programs'),
+                  style: TextButton.styleFrom(
+                    padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                    backgroundColor: MyColors.blue_color,
+                  ),
+                  child: Text("Let's train!", style: TextStyle(
+                    color: Colors.white,
+
+                  ),),),
+              Container(
+                margin: EdgeInsets.all(32),
+                child: Text("Your results", style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 40,
+                ),),
+              ),
+
+            ],
+          ),
+        ),
       ),
       bottomNavigationBar: BottomAppBar(
         child: Row(
